32b4efd4f59430daf65d47b4e2938cadb11b41f2
Le Dinh Hieu <dhgamer1811@gmail.com>
2025-10-22 15:06:21 +0700

    add reader class, delete staff class

--- include/reader.h
+++ include/reader.h
@@ -1,11 +1,29 @@
 #pragma once
 
 #include <string>
+#include <vector>
+
+#include "book.h"
 
 class Reader {
-private:
+ private:
+  std::string id;
   std::string name;
-  std::string year;
+  int year;
+  std::vector<Book*> borrowedBook;
 
-public:
+ public:
+  Reader();
+  Reader(std::string id, std::string name, int year, std::string borrowDate,
+         std::string dueDate, std::vector<Book*> borrowedBook);
+
+  std::string getId();
+  std::string getName();
+  int getYear();
+  std::vector<Book*> Reader::getBorrowedBook();
+
+  void setId(std::string id);
+  void setName(std::string name);
+  void setYear(int year);
+  void setBorrowedBook(std::vector<Book*> borrowedBook);
 };
